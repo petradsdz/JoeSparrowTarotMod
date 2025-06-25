81549db84f22b965733ba0f9b0bb57ce0d47e616
Petradsdz <petradsdz@gmail.com>
2025-06-25 17:16:53 +0100

    Add files via upload

---
+++ Joe Sparrow Tarot/README.md
@@ -0,0 +1,11 @@
+# Aikoyori Paint Job
+A Malverk mod featuring my art for jokers for Balatro
+
+# Download instruction?
+Install Lovely, Steamodded, and get [Malverk](https://balatromods.miraheze.org/wiki/Malverk), then click the green [Code] button above then in that zip
+
+Now copy the **one** folder in the zip to %AppData%\Balatro\Mods\
+
+# License?
+
+Code is GPLv3 and art is CC-BY-NC-SA 4.0 as per my usual
---
+++ Joe Sparrow Tarot/main.lua
@@ -0,0 +1,41 @@
+AltTexture{
+    key = 'tarot',
+    set = 'Tarot',
+    path = 'JoeSparot.png',
+    original_sheet = true,
+    keys = {
+        'c_fool',
+        'c_magician',
+        'c_high_priestess',
+        'c_empress',
+        'c_emperor',
+        'c_heirophant',
+        'c_lovers',
+        'c_chariot',
+        'c_justice',
+        'c_hermit',
+        'c_wheel_of_fortune',
+        'c_strength',
+        'c_hanged_man',
+        'c_death',
+        'c_temperance',
+        'c_devil',
+        'c_tower',
+        'c_star',
+        'c_moon',
+        'c_sun',
+        'c_judgement',
+        'c_world'
+    },
+}
+
+TexturePack{
+    key = 'balatretex',
+    textures = {
+        'sparrow_tarot'
+    },
+    loc_txt = {
+        name = 'Sparrow Tarot',
+        text = {'Joe Sparrow replacement for Tarot cards'}
+    }
+}
